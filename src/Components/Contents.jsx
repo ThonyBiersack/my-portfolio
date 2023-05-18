fe1d6119b2cbdc74ea7289e9d928f8cf4bafbab7
ThonyBiersack <91604506+ThonyBiersack@users.noreply.github.com>
2023-05-18 23:03:22 +0700

    fix multiple page and h2 in contents

--- src/Components/Contents.jsx
+++ src/Components/Contents.jsx
@@ -5,25 +5,23 @@ import Welcome from './Welcome';
 import Blog from './Blog';
 import Kata from './Notfound'
 
-    export default function MyContent(){
-        return(
-            <div className='Content'>
-                <div>
-                    <h2>
-                        <Routes>
-                            <Route path='/' element={<Welcome />} />
-                            <Route path='/carkus' element={<Carkus />} />
-                            <Route path='/blog' element={<Blog />} />
-                            <Route path='*' element={<Kata/>} />
-                        </Routes>
-                    </h2>
-                </div>
-                <div>
-                    <h2>Project</h2>
-                        <p><Link to="/blog">Mine Blog</Link></p>
-                        <p><Link to="/carkus">Carkus</Link></p>
-
-                </div>
+export default function MyContent(){
+    return(
+        <div className='Content'>
+            <div>
+                <Routes>
+                    <Route path='/' element={<Welcome />} />
+                    <Route path='/carkus' element={<Carkus />} />
+                    <Route path='/blog' element={<Blog />} />
+                    <Route path='/*' element={<Kata/>} />
+                </Routes>
             </div>
+            <div>
+                <h2>Project</h2>
+                    <p><Link to="/blog">Mine Blog</Link></p>
+                    <p><Link to="/carkus">Carkus</Link></p>
+
+            </div>
+        </div>
         )
     }
